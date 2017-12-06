5744afcd5c65a7cc5d38ea0d20ed2179b01dc20d
Domenic <domenicbove@gmail.com>
2017-12-05 23:12:43 -0800

    removing unused param

--- mainPipeline.groovy
+++ mainPipeline.groovy
@@ -1,4 +1,4 @@
-// Load mplat-openshift-pipeline Shared Library
+// Load openshift-pipeline Shared Library
 library identifier: 'openshift-pipeline@master', retriever: modernSCM([$class: 'GitSCMSource',
    remote: 'https://github.com/domenicbove/openshift-pipeline.git']) _
 
@@ -8,7 +8,6 @@ mainPipeline {
     templateGitRepoUrl = "https://github.com/domenicbove/openshift-templates.git"
     templateGitTag = "master"
     ocpUrl = "master1.fismobile.net"
-    ocpAuthTokenCredentialId ="OCP_SA_CICD_JENKINS"
     buildProject = "demo-build"
     testProject = "demo-test"
     prodProject = "demo-prod"
